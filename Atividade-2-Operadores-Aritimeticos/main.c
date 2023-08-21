c713fa19dc606cb937bcb8ebc04218467c73f287
Sara Cristina <94648070+SarinhaX4@users.noreply.github.com>
2023-08-21 11:27:30 -0300

    ativ-2-prazo_21/08

---
+++ Atividade-2-Operadores-Aritimeticos/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// 1) Escreva um programa que receba dois números e mostre a soma, a subtração, a multiplicação e a divisão desses números.
+
+// main(){
+//   int num1, num2, resultSoma, resultSubtracao, resultMultiplicacao;
+//   float resultDivisao;
+
+//   printf("Digite um número\n");
+//   scanf("%d", &num1);
+
+//   printf("Digite outro número\n");
+//   scanf("%d", &num2);
+
+//   resultSoma = num1 + num2;
+//   resultSubtracao = num1 - num2;
+//   resultMultiplicacao = num1 * num2;
+//   resultDivisao = (float)num1 / num2;
+
+//   printf("\nO resultado da soma é: %d", resultSoma);
+//   printf("\nO resultado da subtração é: %d", resultSubtracao);
+//   printf("\nO resultado da Multiplicação é: %d", resultMultiplicacao);
+//   printf("\nO resultado da divisão é: %.1f", resultDivisao);
+// }
