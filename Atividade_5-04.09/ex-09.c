8cd852cf364d2c9fe69b2d3f7c6448b2244465f0
Sara Cristina <94648070+SarinhaX4@users.noreply.github.com>
2023-09-13 19:01:18 -0300

    Update ex-09.c

--- Atividade_5-04.09/ex-09.c
+++ Atividade_5-04.09/ex-09.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
 
-//Uma empresa decide dar um aumento aos seus funcionários de acordo com uma tabela que considera o salario atual e o tempo de serviço de cada funcionário. Os funcionários com menor salario terão um aumento proporcionalmente maior do que os funcionários  com um salario maior, e conforme o tempo de serviço na empresa, cada funcionário irá receber um bônus adicional de salário. Faça um programa que leia:
+//Faça um programa que leia: • o valor do salario atual do funcionário;  • o tempo de serviço desse funcionário na empresa (número de anos de trabalho na empresa).Use as tabelas abaixo para calcular o salario reajustado deste funcionário e imprima o valor do salario final reajustado, ou uma mensagem caso o funcionário não tenha direito a nenhum aumento. 
 
-main{
-  int salarioAtual, temServico, novoSalario, ajuste;
 
-  printf("Digite o seu salário atual:");
-  scanf("%d", &salarioAtual);
 
-  printf("Digite o seu tempo de serviço em meses:");
-  scanf("%d", &tempServico);
+main() {
 
-  if( salarioAtual <= 500 && tempServico < 12 ){
-    ajuste = 500*(0.25);
-    NovoSalario = ajuste + salarioAtual;
-    printf("O seu novo salário será de: %d", NovoSalario);
-  } else if(salarioAtual >= 1000 && tempServico >){
+    float tempo;
+    float salarioAtual, salarioReajustado = 0, bonus, salarioFinal;
+
+    printf("Digite o salario");
+    scanf("%f", &salarioAtual);
     
-  }
-  
+    printf("DIgite o tempo de servico");
+    scanf("%f", &tempo);
+
+    if(salarioAtual < 500){
+        salarioReajustado = salarioAtual * 1.25;
+    }else if(salarioAtual > 500 && salarioAtual < 1000){
+        salarioReajustado = salarioAtual * 1.2;
+    }else if(salarioAtual > 1000 && salarioAtual < 1500){
+        salarioReajustado = salarioAtual * 1.15;
+    }else if(salarioAtual > 1500 && salarioAtual < 2000){
+        salarioReajustado = salarioAtual * 1.1;
+    }else{
+        salarioReajustado = salarioAtual;
+    }
+
+    if(tempo < 1){
+        bonus = 0;
+    }else if(tempo >= 1 && tempo < 3){
+        bonus = 100;
+    }else if(tempo >= 3 && tempo < 6){
+        bonus = 200;
+    }else if(tempo >= 6 && tempo < 10){
+        bonus = 300;
+    }else{
+        bonus = 500;
+    }
+
+    salarioFinal = salarioReajustado + bonus;
+
+    if(salarioFinal == salarioAtual){
+        printf("Não teve aumento");
+    }else{
+        printf(" teve aumento");
+        printf("Salario final: %f", salarioFinal);
+    }
 }
